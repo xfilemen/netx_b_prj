2439568f566ec86a495f8b9973103f239710492e
tjdghks7373 <86451041+tjdghks7373@users.noreply.github.com>
2024-08-29 17:43:02 +0900

    테스트

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function HomePage() {
 
   return (
     <div>
-      <h1>김성환안천재</h1>
+      <h1>김성환 천재</h1>
       <ul>
         {users.map((user) => (
           <li key={user.id}>
