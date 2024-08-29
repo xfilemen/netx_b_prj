53ff2335375002af8cea5d877475bc0fd94911d6
tjdghks7373 <86451041+tjdghks7373@users.noreply.github.com>
2024-08-29 17:02:47 +0900

    소스 수정1

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,29 @@
-<h1>김성환</h1>
+import { useState, useEffect } from 'react';
+import './css/base.css';
+
+export default function HomePage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch('/api/getUsers');
+      const data = await response.json();
+      setUsers(data);
+    }
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>김성환</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}-{user.age}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
