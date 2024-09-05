bdf2172d4b9529f9fc0f62b9b017f8f2f00c7463
KIM <tjdghks7373@naver.com>
2024-09-05 15:16:23 +0900

    css 테스트

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import { useState, useEffect } from 'react';
-//import styles from "../css/base.module.css";
+import styles from '../css/base.module.css';
 
-function HomePage() {
+export default function Home() {
   return (
-    <div>
-      <h1 style="font-size:10px;font-weight:bold;">김성환 천재12</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>김성환 천재12</h1>
     </div>
   );
 }
-
-export default HomePage;
