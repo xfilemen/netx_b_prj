d4207ffee96f9c2ec3ab87fede80c527becbea9b
KIM <tjdghks7373@naver.com>
2024-09-05 17:45:04 +0900

    최종 라스트 수정

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import { useState, useEffect } from 'react';
+import styles from './styles/main.module.css';
+
 export default function Home() {
-    return <h1>Welcome to Next.js!</h1>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>유현님 바보</h1>
+    </div>
+  );
 }
