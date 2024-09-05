31c036d2dad71941f5f2166f46da96690dd6d2c2
KIM <tjdghks7373@naver.com>
2024-09-05 14:40:24 +0900

    테스트

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,10 @@
 import { useState, useEffect } from 'react';
-import '/css/base.module.css'
+//import '/css/base.module.css'
 
 export default function HomePage() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    async function fetchUsers() {
-      const response = await fetch('/api/getUsers');
-      const data = await response.json();
-      setUsers(data);
-    }
-
-    fetchUsers();
-  }, []);
-
   return (
     <div>
       <h1>김성환 천재12</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name} - {user.email}-{user.age}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
