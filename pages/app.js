1649f8803db5468df1f912f7206221e8421e404e
KIM <tjdghks7373@naver.com>
2024-09-05 15:21:55 +0900

    css 테스트

--- pages/app.js
+++ pages/app.js
@@ -1,4 +1,4 @@
-import '../css/base.css';
+import '../pages/css/base.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import styles from '../css/base.module.css';
+import styles from '../pages/css/base.module.css';
 
 export default function Home() {
   return (
