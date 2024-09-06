51891c72d05f621ce552ac7d6e93c6d40655a663
KIM <tjdghks7373@naver.com>
2024-09-06 17:19:40 +0900

    로그인 페이지 테스트 2

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,10 @@ import Image from 'next/image';
 export default function LoginPage() {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.loginbg}>
         <Image 
           src="/images/loginbg.png" // 이미지 경로
           alt="로그인 페이지"
-          width={500}
-          height={300}
         />
       </div>
     </div>
