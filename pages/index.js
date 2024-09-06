90673f17be2fb4d234363f37c62f29097cf3a0ac
KIM <tjdghks7373@naver.com>
2024-09-06 17:30:27 +0900

    로그인 페이지 테스트 7

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
 import styles from './styles/main.module.css';
 import Image from 'next/image';
-import loginImage from '/images/login/loginbg.png';
 
 export default function LoginPage() {
   return (
     <div className={styles.container}>
       <div className={styles.loginbg}>
         <Image 
-          src={loginImage} // 이미지 경로
+          src="/images/login/loginbg.png" // 이미지 경로
           alt="로그인 페이지"
         />
       </div>
