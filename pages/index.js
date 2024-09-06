8fa95abb756172dfe78e3cd19d2c200a8c70e2ed
KIM <tjdghks7373@naver.com>
2024-09-06 16:47:58 +0900

    로그인 페이지 이미지 적용

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react';
 import styles from './styles/main.module.css';
+import loginImage from './images/loginbg.png'; // 이미지 파일 경로
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>유현님 바보</h1>
+      <div className={styles.loginimg}>
+        <Image
+          src={loginImage} // 이미지 경로
+          alt="로그인 페이지"
+        />
+      </div>
     </div>
   );
 }
