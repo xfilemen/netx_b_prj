b64a484cd74263bbd25422a86f114ab386f2cc30
KIM <tjdghks7373@naver.com>
2024-09-13 15:58:32 +0900

    디테일 페이지 생성(테스트)

---
+++ pages/detail.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+import styles from './styles/main.module.css';
+import Image from 'next/image';
+
+export default function LoginPage() {
+  return (
+    <div className={styles.container}>
+      <div>디테일</div>
+    </div>
+  );
+}
