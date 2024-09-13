af83c0ed5617dc3a5c5bfc48439449371dc9575d
KIM <tjdghks7373@naver.com>
2024-09-13 17:02:38 +0900

    테스트123

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import styles from './styles/main.module.css';
 import Image from 'next/image';
 
-export default function LoginPage() {
+export default function LoginPage() { 
   return (
     <div className={styles.container}>
       <div className={styles.loginbg}>
