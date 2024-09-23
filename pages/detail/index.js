ff4342b654ef47e0527fd1910a26fafc40ce9d06
KIM <tjdghks7373@naver.com>
2024-09-23 15:27:50 +0900

    정규인력 페이지 생성

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,7 +1,14 @@
-export default function Page() {
+import Image from 'next/image';
+
+export default function RegularPage() {
   return (
     <div>
-      <h1>Deteil Page</h1>
+      <h1>
+        <Image 
+            src="/images/detail/diware_logo_img.png"
+            alt="DIware"
+          />
+      </h1>
       <p>This is the deteil page.</p>
     </div>
   );
