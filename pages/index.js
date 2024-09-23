3144af4459dff71fea172c73fd55c75da94704d6
KIM <tjdghks7373@naver.com>
2024-09-23 13:38:33 +0900

    테스트

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import styles from './styles/main.module.css';
-import Image from 'next/image';
+// import { useState, useEffect } from 'react';
+// import Link from 'next/link';
+// import styles from './styles/main.module.css';
+// import Image from 'next/image';
 
-export default function LoginPage() { 
+// export default function LoginPage() { 
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.loginbg}>
+//         <Link href="/detail" onclick="">
+//           <Image 
+//             src="/images/login/loginbg.png"
+//             alt="로그인 페이지"
+//           />
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
+
+import Link from 'next/link';
+
+export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.loginbg}>
-        <Link href="/detail">
-          <Image 
-            src="/images/login/loginbg.png"
-            alt="로그인 페이지"
-          />
-        </Link>
-      </div>
+    <div>
+      <h1>홈 페이지</h1>
+      <Link href="/detail">소개 페이지로 이동</Link>
     </div>
   );
 }
