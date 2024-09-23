b5a8bd0758c6bdfd759fa0603e78dddd9a3f0b0d
KIM <tjdghks7373@naver.com>
2024-09-23 15:13:41 +0900

    페이지 초기설정

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,19 @@
-// import { useState, useEffect } from 'react';
-// import Link from 'next/link';
-// import styles from './styles/main.module.css';
-// import Image from 'next/image';
-
-// export default function LoginPage() { 
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.loginbg}>
-//         <Link href="/detail" onclick="">
-//           <Image 
-//             src="/images/login/loginbg.png"
-//             alt="로그인 페이지"
-//           />
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
-
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import styles from './styles/main.module.css';
+import Image from 'next/image';
 
-export default function Page() {
+export default function LoginPage() { 
   return (
-    <div>
-      <h1>홈 페이지</h1>
-      <Link href={'/detail'}>소개 페이지로 이동</Link>
+    <div className={styles.container}>
+      <div className={styles.loginbg}>
+        <Link href="/detail" onclick="">
+          <Image 
+            src="/images/login/loginbg.png"
+            alt="로그인 페이지"
+          />
+        </Link>
+      </div>
     </div>
   );
 }
