553750d4187a3a6bee652f2f094811147f1d1ec4
KIM <tjdghks7373@naver.com>
2024-09-24 17:49:12 +0900

    헤더 컴포넌트 수정

--- components/header/index.js
+++ components/header/index.js
@@ -4,20 +4,18 @@ import Image from 'next/image';
 
 export default function HeaderPage() {
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <h1>
-          <Image 
-              src="/images/detail/diware_logo_img.png"
-              alt="DIware"
-              width={91}
-              height={22}
-            />
-        </h1>
-        <div className={styles.navigation}>
-          <NaviList />
-        </div>
-      </div>      
+    <div className={styles.header}>
+      <h1>
+        <Image 
+            src="/images/detail/diware_logo_img.png"
+            alt="DIware"
+            width={91}
+            height={22}
+          />
+      </h1>
+      <div className={styles.navigation}>
+        <NaviList />
+      </div>
     </div>
   );
 }
