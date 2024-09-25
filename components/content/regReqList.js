84e584c422c6c5699e22ce1b5fa507393c7fb8ab
KIM <tjdghks7373@naver.com>
2024-09-25 16:20:49 +0900

    퍼블작업(정규인력 요청 내역)

--- components/content/regReqList.js
+++ components/content/regReqList.js
@@ -1,33 +1,58 @@
+import React, { Component } from 'react';
 import styles from '/pages/styles/detail.module.css';
-import Image from 'next/image';
+import requstData from '/data/regRequstList.json';
+import Link from 'next/link';
 
-export default function RegularPage() {
-  return (
-    <div className={styles.content}>
-      <div className={styles.topbanner}>
-        <Image 
-          src="/images/detail/TopVisual.png"
-          alt="효율적인 인력배치 언제든 문의하세요"
-          width={1440}
-          height={150}
-        />
+class ItemList extends Component {
+  getStatusText(status) {
+    switch (status) {
+      case 'register':
+        return '접수';
+      case 'progress':
+        return '진행';
+      case 'cancel':
+        return '취소';
+      case 'return':
+        return '반려';
+      case 'complete':
+        return '완료';
+      default:
+        return status; // 다른 상태는 원래 값을 사용
+    }
+  }
+
+  getStatusClass(status) {
+    switch (status) {
+      default:
+        return styles[status];
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.list_items}>
+        <ul>
+          {requstData.map((item) => (
+            <li key={item.id}>
+              <Link href={''}>
+                <div className={`${styles.state} ${this.getStatusClass(item.status)}`}>
+                  {this.getStatusText(item.status)}
+                </div>
+                <div className={styles.section}>
+                  <p className={styles.tit_tx}>{item.title}</p>
+                  <div className={styles.tx_info}>
+                    <div className={styles.priority}>우선순위 <span>{item.priority}</span></div>
+                    <div className={styles.date}>요청일 {item.date}</div>
+                    <div className={styles.num}>요청인원 {item.requestedBy}</div>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
-      <div className={styles.wrap}>
-          <div className={styles.left_section}>
-            <div className={styles.title}>
-              <h2>정규인력 요청·내역</h2>
-              <div className={styles.btn}>
-                <button>Filter</button>
-              </div>
-            </div>
-            <div className={styles.item_list}>
-              
-            </div>
-          </div>
-          <div className={styles.right_section}>
-            right
-          </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default ItemList;
