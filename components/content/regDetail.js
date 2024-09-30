a86bd46f5df0e6afd8309987cd3c87b903269007
KIM <tjdghks7373@naver.com>
2024-09-30 17:55:02 +0900

    정규인력 요청 내역

--- components/content/regDetail.js
+++ components/content/regDetail.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import styles from '/pages/styles/detail.module.css';
+import Image from 'next/image';
 
 
 class RegDetail extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
 
   getStatusText(status) {
     switch (status) {
@@ -42,8 +48,13 @@ class RegDetail extends Component {
     }
   }
 
+  toggleAccordion = () => {
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+  };
+
   render() {
     const { item } = this.props;
+    const { isOpen } = this.state;
     return (
       item && (
         <div className={styles.detail_content}>
@@ -56,6 +67,30 @@ class RegDetail extends Component {
               <div className={styles.date}>요청일 {item.date}</div>
               <div className={styles.num}>요청인원 {item.requestedBy}명</div>
             </div>
+            <div className={styles.accordion}>
+              <div className={styles.title} onClick={this.toggleAccordion}>
+                  <h3>
+                      <Image 
+                          src="/images/detail/ico_info.png"
+                          alt="요청 기본 정보"
+                          width={46}
+                          height={46}
+                      />
+                      요청 기본 정보</h3>
+                  <span>
+                    <Image 
+                      src={isOpen ? "/images/detail/ico_ac_up.png" : "/images/detail/ico_ac_down.png"}
+                      alt={isOpen ? "닫기" : "열기"}
+                      width={12}
+                      height={6}
+                      className={styles.arrowIcon}
+                    />
+                  </span>
+              </div>
+              {!isOpen && <div className={styles.content}>
+                <div className={styles.line}>요청명 <span>{item.requestName}</span></div>
+              </div>}
+          </div>
         </div>
       )
     );
