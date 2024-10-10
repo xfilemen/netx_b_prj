f19bd3c6aac1fd225fb4ce0253e4fcd5c3ceac65
KIM <tjdghks7373@naver.com>
2024-10-10 13:57:14 +0900

    정규인력 요청·내역 작업

---
+++ app/not-found.jsx
@@ -0,0 +1,24 @@
+import style from '/app/styles/detail.module.css';
+import Header from '/app/components/header';
+import Link from 'next/link';
+
+export default function notFoundPage() {
+  return (
+    <div className={style.container}>
+        <Header />
+        <div className={style.content}>
+            <div className={style.wrap}>
+                <div className={style.not_page}>
+                    <h2>다시 한번 확인해 주세요!</h2>
+                    <p>지금 입력하신 주소의 페이지는 삭제되었거나 다른 페이지로 변경되었습니다.<br />주소를 다시 확인해 주세요.</p>
+                    <div className={style.btn}>
+                        <Link href={'/detail'}>
+                            메인
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
