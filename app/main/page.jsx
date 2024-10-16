cf50b770c6af8d5547f324d01d4c8506af1fa2c7
KIM <tjdghks7373@naver.com>
2024-10-16 17:22:41 +0900

    메인 페이지 작업

--- app/main/page.jsx
+++ app/main/page.jsx
@@ -1,4 +1,6 @@
 import styles from '/app/styles/main.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function MainPage() {
     return (
@@ -12,6 +14,43 @@ export default function MainPage() {
                         <li>진행 현황 <span className={styles.num}>1</span></li>
                         <li>취소/반려 현황 <span className={styles.num}>1</span></li>
                         <li>완료 현황 <span className={styles.num}>1</span></li>
+                    </ul>
+                </div>
+                <div className={styles.link_menu}>
+                    <ul>
+                        <li>
+                            <Link href={'/'}>
+                                <Image 
+                                    src="/images/main/ico_req.png"
+                                    alt="인력 요청 아이콘"
+                                    width={46}
+                                    height={46}
+                                />
+                                <span className={styles.tx}>정규/BP<br/>인력 요청하기</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={'/'}>
+                                <Image 
+                                    src="/images/main/ico_reg.png"
+                                    alt="인력 요청 아이콘"
+                                    width={46}
+                                    height={46}
+                                />
+                                <span className={styles.tx}>정규/BP 인력<br />요청 내역 확인하기</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={'/'}>
+                                <Image 
+                                    src="/images/main/ico_noti.png"
+                                    alt="인력 요청 아이콘"
+                                    width={46}
+                                    height={46}
+                                />
+                                <span className={`${styles.tx} ${styles.line1}`}>공지사항</span>
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
