4744c059480ff7c83906d1cf3c56e35222adc908
KIM <tjdghks7373@naver.com>
2024-10-17 16:11:15 +0900

    메인 페이지 작업

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -10,7 +10,7 @@ export default function HeaderPage() {
   const pathname = usePathname();
   const showNaviList = pathname !== '/main';
   const { data: session } = useSession();
-  let userInfo = {};
+  const userInfo = {};
   userInfo = session?.user || {};
 
   const [isUserInfoVisible, setUserInfoVisible] = useState(false); // 토글 상태 관리
