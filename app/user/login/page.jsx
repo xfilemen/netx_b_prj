f3d8e08f436f9916d3e4aefb7375871cb762ea5b
KIM <tjdghks7373@naver.com>
2024-10-18 15:38:42 +0900

    로그인 페이지 수정

--- app/user/login/page.jsx
+++ app/user/login/page.jsx
@@ -30,50 +30,47 @@ export default function LoginPage() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.loginbg}>
-        <Image 
-          src="/images/login/left_login_bg.png"
-          alt="WORKFORCE REQUEST MANAGER SYSTEM"
-          width={840}
-          height={919}
-        />
-      </div>
-      <div className={styles.loginSection}>
-        <div className={styles.content}>
-            <form onSubmit={handleSubmit}>
-              <h1>Login</h1>
-              <input
-                type="text"
-                placeholder="CJ World 계정"
-                value={cj_id}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-              <input
-                type="password"
-                placeholder="비밀번호"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <div className={styles.saveid}>
+      <div className={styles.section}>
+        <div className={styles.loginbg}>
+          <img src="/images/login/left_login_bg.jpg" alt="WORKFORCE REQUEST MANAGER SYSTEM"/>
+        </div>
+        <div className={styles.loginSection}>
+          <div className={styles.content}>
+              <form onSubmit={handleSubmit}>
+                <h1>Login</h1>
                 <input
-                  type="checkbox"
-                  placeholder="아이디 저장"
-                /> <span className={styles.saveidtx}>아이디 저장</span>
+                  type="text"
+                  placeholder="CJ World 계정"
+                  value={cj_id}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+                <input
+                  type="password"
+                  placeholder="비밀번호"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <div className={styles.saveid}>
+                  <input
+                    type="checkbox"
+                    placeholder="아이디 저장"
+                  /> <span className={styles.saveidtx}>아이디 저장</span>
+                </div>
+                <button type="submit">로그인</button>
+              </form>
+              <div className={styles.join_section}>
+                <ul>
+                  <li>계정찾기</li>
+                  <li className={styles.line}>ㅣ</li>
+                  <li>비밀번호 찾기</li>
+                  <li className={styles.line}>ㅣ</li>
+                  <li>계정생성 요청</li>
+                </ul>
               </div>
-              <button type="submit">로그인</button>
-            </form>
-            <div className={styles.join_section}>
-              <ul>
-                <li>계정찾기</li>
-                <li className={styles.line}>ㅣ</li>
-                <li>비밀번호 찾기</li>
-                <li className={styles.line}>ㅣ</li>
-                <li>계정생성 요청</li>
-              </ul>
             </div>
-          </div>
+        </div>
       </div>
     </div>
   );
