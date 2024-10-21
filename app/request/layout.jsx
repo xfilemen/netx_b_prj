5ef2abd7c0ef90b733b34607a2fd61a229f89852
KIM <tjdghks7373@naver.com>
2024-10-21 15:34:27 +0900

    정규 / bp 인력 페이지 (작업중)

--- app/request/layout.jsx
+++ app/request/layout.jsx
@@ -2,6 +2,11 @@ import styles from '../styles/detail.module.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
+export const metadata = {
+  title: '인력요청시스템',
+}
+
+
 export default function RegistLayout({ children }) {
   return (
     <div className={styles.container}>
--- app/request/page.jsx
+++ app/request/page.jsx
@@ -1,13 +1,59 @@
-import styles from '/app/styles/main.module.css';
+"use client";
 
-export const metadata = {
-  title: '인력요청시스템',
-}
+import React, { useState } from 'react';
+import styles from '../styles/request.module.css';
+import Image from 'next/image';
 
 export default function RegPage() { 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className={styles.container}>
-      등록 페이지
+    <div className={styles.content}>
+      <div className={styles.topbanner}>
+        <Image 
+          src="/images/detail/TopVisual.png"
+          alt="효율적인 인력배치 언제든 문의하세요"
+          width={1440}
+          height={150}
+        />
+      </div>
+      <div className={styles.wrap}>
+        <h2>정규/BP 인력 요청</h2>
+        <div className={styles.accordion}>
+          <div className={styles.title} onClick={toggleAccordion}>
+            <h3>
+              <Image
+                src="/images/detail/ico_info.png"
+                alt="요청 기본 정보"
+                width={46}
+                height={46}
+              />
+              요청 기본 정보
+            </h3>
+            <span>
+              <Image
+                src={isOpen ? "/images/detail/ico_ac_up.png" : "/images/detail/ico_ac_down.png"}
+                alt={isOpen ? "닫기" : "열기"}
+                width={12}
+                height={6}
+                className={styles.arrowIcon}
+              />
+            </span>
+          </div>
+          {!isOpen && (
+            <div className={styles.content}>
+              <div className={styles.item}>
+                <span>등급</span>
+                <input type="text" placeholder="ex. CJ PAY Back-End 개발 or CJ ENM 차세대 K-POP 플랫폼 구축" />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
