e6852ed41f7f000c5b24c193afbeb36e6de9d899
KIM <tjdghks7373@naver.com>
2024-11-04 13:16:24 +0900

    상세 진행 현황

--- app/detail/regReqDetail.jsx
+++ app/detail/regReqDetail.jsx
@@ -97,6 +97,35 @@ export default function RegDetail({ item }) {
                 </a>
               </div>
             </div>
+            <div className={styles.content}>
+                <ul>
+                  <li>
+                    <div className={styles.line}><span className={styles.name}>[처리자] 디아이웨어 김열정님</span> | 2024-07-30 15:00</div>
+                    <div>❗ 정규 인력 요청 완료</div>
+                  </li>
+                  <li>
+                    <div className={styles.line}><span className={styles.name}>[처리자] 디아이웨어 김열정님</span> | 2024-07-30 15:00</div>
+                    <div>✍ 김철수님 9/30(월) 채용 확정 </div>
+                  </li>
+                  <li>
+                    <div className={styles.line}><span className={styles.name}>[처리자] 디아이웨어 김열정님</span> | 2024-07-30 15:00</div>
+                    <div>✍ 후보 1명 (김철수님) 9/12(목) 면접 예정 </div>
+                  </li>
+                  <li>
+                    <div className={styles.line}><span className={styles.name}>[처리자] 디아이웨어 김열정님</span> | 2024-07-30 15:00</div>
+                    <div>✍ 최성실님에게 정규직 프로필 요청 후 회신 <br/>
+                    대기중 최성실님에게 정규직 프로필 요청 후 회신 대기중 최성실님에게 정규직 프로필 요청 후 회신 대기중 최성실님에게 정규직 프로필 요청 후 회신 대기중 </div>
+                  </li>
+                  <li>
+                    <div className={styles.line}><span className={styles.name}>[요청자] 디아이웨어 김열정님</span> | 2024-07-30 15:00</div>
+                    <div>❗ 정규 인력 요청 진행</div>
+                  </li>
+                </ul>
+              </div>
+              <div className={styles.comment_section}>
+                <textarea placeholder="코멘트를 입력해 주세요." name="" id=""></textarea>
+                <button>comment</button>
+              </div>
           </div>
         </div>
         {/* 기본 정보 아코디언 */}
